cf8a74c64da6d370f08b3a481e818bee235d9e5d
A2ZTestAutomation <79190914+A2ZTestAutomation@users.noreply.github.com>
2022-11-09 12:53:48 +0530

    Modified by Tester2

--- src/test/java/testScripts/SampleOneTest.java
+++ src/test/java/testScripts/SampleOneTest.java
@@ -11,6 +11,7 @@ import org.testng.annotations.Test;
 		  public void searchJenkinsTest() {
 			 System.out.println("Jenkins Test");
 			 System.out.println("Modified for Github intgr.....")
+				  System.out.println("Modified by Tester2.....")
 		  }
 		@Test
 		  public void searchMavenTest() {
